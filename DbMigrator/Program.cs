495bd3371833379d669fa4b6c9ce0cd00d261739
Oliver Forral <intrepion@gmail.com>
2021-07-07 17:13:32 -0700

    Step 2 - Create DbUp Console application

--- DbMigrator/Program.cs
+++ DbMigrator/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-
+using System.Linq;
+using System.Reflection;
+using DbUp;
+	
 namespace DbMigrator
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var connectionString =
+                args.FirstOrDefault()
+                ?? "Host=localhost;User Id=postgres;Password=Secret!Passw0rd;Database=crazy_database;Port=5432";
+            EnsureDatabase.For.PostgresqlDatabase(connectionString);
+            var upgrader = DeployChanges.To
+                .PostgresqlDatabase(connectionString)
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                .LogToConsole()
+                .Build();
+            var result = upgrader.PerformUpgrade();
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+                return -1;
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(value: "Success!");
+            Console.ResetColor();
+            return 0;
         }
     }
 }
