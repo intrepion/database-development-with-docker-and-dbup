ac16a95a7a56536706f02ce271b1d8959d98af92
Oliver Forral <intrepion@gmail.com>
2021-07-07 17:33:35 -0700

    Added scripts as embedded resources

---
+++ DbMigrator/SqlScripts/001_AddTable_Customer.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Customers (
+    Id int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255)
+);
---
+++ DbMigrator/SqlScripts/002_FillSampleData.sql
@@ -0,0 +1,13 @@
+INSERT INTO Customers VALUES
+(
+    1,
+    'Gerard',
+    'Thomas',
+    'Nowhere 22/11'
+),
+(
+    2,
+    'Newman',
+    'Oldman',
+    'Somwhere 2/12'
+);
